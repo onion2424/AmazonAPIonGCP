c8cbcc2b367fc1261a9105f03c06b405f589484f
翔 杉原 <s-sugihara@danknet.co.jp>
2024-08-04 04:26:07 +0900

    M_Transaction修正

--- src/FireStoreAPI/Collection/M_Transaction/index.js
+++ src/FireStoreAPI/Collection/M_Transaction/index.js
@@ -1,7 +1,7 @@
 import FirestoreManager from "../../manager.js"
 import { M_Transaction } from './class.js';
 import { M_Request } from "../M_Request/class.js";
-import { _, dayjs } from "../../../Common/common.js";
+import { _, dayjs, systemInfo } from "../../../Common/common.js";
 
 
 const query = await FirestoreManager.getQuery("M_Request");
@@ -40,11 +40,11 @@ const GetFlatFileAllOrdersDataByOrderDateGeneral = requests.find(r => r.tag == "
                 }
             ],
         requests: [
-            { ref: spAdvertisedProduct.ref, refName: spAdvertisedProduct.refName, settings: { dateback: 60, host: 1 } },
-            { ref: sbCampaigns.ref, refName: sbCampaigns.refName, settings: { dateback: 60, host: 1 } },
-            { ref: sdAdvertisedProduct.ref, refName: sdAdvertisedProduct.refName, settings: { dateback: 60, host: 1 } },
-            { ref: getSalesAndTrafficReport.ref, refName: getSalesAndTrafficReport.refName, settings: { dateback: 0, host: 2 } },
-            { ref: GetFlatFileAllOrdersDataByOrderDateGeneral.ref, refName: GetFlatFileAllOrdersDataByOrderDateGeneral.refName, settings: { dateback: 0, host: 2 } },
+            { ref: spAdvertisedProduct.ref, refName: spAdvertisedProduct.refName, settings: { dateback: 60} },
+            { ref: sbCampaigns.ref, refName: sbCampaigns.refName, settings: { dateback: 60 } },
+            { ref: sdAdvertisedProduct.ref, refName: sdAdvertisedProduct.refName, settings: { dateback: 60 } },
+            { ref: getSalesAndTrafficReport.ref, refName: getSalesAndTrafficReport.refName, settings: { dateback: 0 } },
+            { ref: GetFlatFileAllOrdersDataByOrderDateGeneral.ref, refName: GetFlatFileAllOrdersDataByOrderDateGeneral.refName, settings: { dateback: 0 } },
         ],
         valid: true,
     }
@@ -70,13 +70,13 @@ const GetFlatFileAllOrdersDataByOrderDateGeneral = requests.find(r => r.tag == "
             ],
         schedulize: "FireStoreAPI/Collection/M_Transaction/Status/RegularReport/schedulize",
         requests: [
-            { ref: spAdvertisedProduct.ref, refName: spAdvertisedProduct.refName, settings: { spans: [1, 2, 3, 4, 5, 6, 7, 30], granularity: "DAY"  }},
-            { ref: sbCampaigns.ref, refName: sbCampaigns.refName, settings: { spans: [1, 2, 3, 4, 5, 6, 7, 30], granularity: "DAY"  }},
-            { ref: sdAdvertisedProduct.ref, refName: sdAdvertisedProduct.refName, settings: { spans: [1, 2, 3, 4, 5, 6, 7, 30], granularity: "DAY"  }},
-            { ref: getSalesAndTrafficReport.ref, refName: getSalesAndTrafficReport.refName, settings: { spans: [1, 2, 3, 4, 5, 6, 7, 30], granularity: "DAY"  }},
-            { ref: getMerchantListingsAllData.ref, refName: getMerchantListingsAllData.refName, settings: { spans: [1], granularity: "DAY"  }},
-            { ref: GetFbaMyiAllInventoryData.ref, refName: GetFbaMyiAllInventoryData.refName, settings: { spans: [1], granularity: "DAY"  }},
-            { ref: GetFlatFileAllOrdersDataByOrderDateGeneral.ref, refName: GetFlatFileAllOrdersDataByOrderDateGeneral.refName, settings: { spans: [1, 2, 3, 4, 5, 6, 7, 30], granularity: "DAY"  }}
+            { ref: spAdvertisedProduct.ref, refName: spAdvertisedProduct.refName, settings: { spans: systemInfo.isTest() ? [1] : [1, 2, 3, 4, 5, 6, 7, 30], granularity: "DAY"  }},
+            { ref: sbCampaigns.ref, refName: sbCampaigns.refName, settings: { spans: systemInfo.isTest() ? [1] : [1, 2, 3, 4, 5, 6, 7, 30], granularity: "DAY"  }},
+            { ref: sdAdvertisedProduct.ref, refName: sdAdvertisedProduct.refName, settings: { spans: systemInfo.isTest() ? [1] : [1, 2, 3, 4, 5, 6, 7, 30], granularity: "DAY"  }},
+            { ref: getSalesAndTrafficReport.ref, refName: getSalesAndTrafficReport.refName, settings: { spans: systemInfo.isTest() ? [1] : [1, 2, 3, 4, 5, 6, 7, 30], granularity: "DAY"  }},
+            { ref: getMerchantListingsAllData.ref, refName: getMerchantListingsAllData.refName, settings: { spans: systemInfo.isTest() ? [1] : [1], granularity: "DAY"  }},
+            { ref: GetFbaMyiAllInventoryData.ref, refName: GetFbaMyiAllInventoryData.refName, settings: { spans: systemInfo.isTest() ? [1] : [1], granularity: "DAY"  }},
+            { ref: GetFlatFileAllOrdersDataByOrderDateGeneral.ref, refName: GetFlatFileAllOrdersDataByOrderDateGeneral.refName, settings: { spans: systemInfo.isTest() ? [1] : [1, 2, 3, 4, 5, 6, 7, 30], granularity: "DAY"  }}
         ],
         valid: true,
     }
